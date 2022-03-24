3fe77019565027ffa3b67bdacf67e3b68d140247
sh05158 <sh05158@naver.com>
2022-03-25 01:13:32 +0900

    메인 페이지 개발

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
+import logo from './logo_new.png';
 import './App.css';
+import { Provider } from 'react-redux'
+import { Button } from '@material-ui/core'
+import React, { Component } from 'react';
+import HomeIcon from '@material-ui/icons/Home';
+import Icon from '@material-ui/core/Icon';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import ComputerIcon from '@material-ui/icons/Computer';
+import Grid from '@material-ui/core/Grid';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#337C7E' }
+  }
+});
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> 안녕하세요 저는 우하은입니다 크크루삥뽕
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={logo} className="App-logo" alt="logo" />
+        
+      <MuiThemeProvider theme={theme}>
+      <Button variant="contained" style={{ "fontSize":"50px","min-height": "150px", width: "35%" }} color="primary" size="large">
+        새로 맞추기
+      </Button>
+      <br></br>
+      <Button variant="contained" style={{ "fontSize":"50px","min-height": "150px", width: "35%" }} color="primary" size="large">
+        기존 컴퓨터 업그레이드
+      </Button>
+        </MuiThemeProvider>
+      
+      
+     
+      
+        
       </header>
     </div>
   );
